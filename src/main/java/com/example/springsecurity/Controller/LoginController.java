cc9d5eaf22b9000e299178a91bacca641f7fa9c7
deanyang122 <i64991189@gs.ncku.edu.tw>
2024-03-09 01:03:38 +0800

    add login page and index

---
+++ src/main/java/com/example/springsecurity/Controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.example.springsecurity.Controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getLoginPage(){
+        return "login";
+    }
+
+    @GetMapping("/index")
+    public String home(Model model, Authentication authentication) {
+        model.addAttribute("username", authentication.getName());
+        model.addAttribute("roles", authentication.getAuthorities());
+        return "index";
+    }
+}
--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.springsecurity.config;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.PermissionEvaluator;
@@ -7,10 +10,20 @@ import org.springframework.security.access.expression.method.DefaultMethodSecuri
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import java.io.IOException;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -29,14 +42,51 @@ public class SecurityConfig{
                 .authorizeHttpRequests((authorize) -> authorize
                         // spring security 6 之後 antMatchers(), mvcMathcers(), regexMatchers() 皆 removed
                         // 因此改用 requestMatchers
+                        .requestMatchers("/login").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
                 )
-                .httpBasic(withDefaults())
-                .formLogin(withDefaults());
+                .formLogin(
+//                        withDefaults()
+                        //客製化的login page
+                        formLogin -> formLogin
+                                .loginPage("/login")
+//                               這個也是在6被棄用的寫法
+//                                .defaultSuccessURL("/index", true)
+                                .permitAll()
+//                                基礎使用
+//                                .successHandler(new SimpleUrlAuthenticationSuccessHandler("/index"))
+//                               或者使用以下代碼根據用戶的角色將他們重定向到不同的頁面
+                                .successHandler(new AuthenticationSuccessHandler() {
+
+                                    @Override
+                                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                                        Authentication authentication) throws IOException, ServletException {
+
+                                        String targetUrl = "/index";
+
+                                        if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"))) {
+                                            targetUrl = "/admin";
+                                        }
+
+                                        response.sendRedirect(targetUrl);
+                                    }
+                                })
+                );
         // @formatter:on
         return http.build();
+    }
+
+    // 範例user, 初期開發時可用
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
@@ -44,15 +94,15 @@ public class SecurityConfig{
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public MethodSecurityExpressionHandler expressionHandler() {
-        DefaultMethodSecurityExpressionHandler handler = new DefaultMethodSecurityExpressionHandler();
-        handler.setPermissionEvaluator(customPermissionEvaluator());
-        return handler;
-    }
-
-    @Bean
-    public PermissionEvaluator customPermissionEvaluator() {
-        return new CustomPermissionEvaluator();
-    }
+//    @Bean
+//    public MethodSecurityExpressionHandler expressionHandler() {
+//        DefaultMethodSecurityExpressionHandler handler = new DefaultMethodSecurityExpressionHandler();
+//        handler.setPermissionEvaluator(customPermissionEvaluator());
+//        return handler;
+//    }
+//
+//    @Bean
+//    public PermissionEvaluator customPermissionEvaluator() {
+//        return new CustomPermissionEvaluator();
+//    }
 }
